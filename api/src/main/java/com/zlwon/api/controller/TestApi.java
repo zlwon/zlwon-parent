0d88d0be79a545c50927ca0e9c5b0c802d1bc4a7
yangy <871271816@qq.com>
2018-03-29 13:48:25 +0800

    压力测试  杨宇  2018.3.29

--- api/src/main/java/com/zlwon/api/controller/TestApi.java
+++ api/src/main/java/com/zlwon/api/controller/TestApi.java
@@ -1,12 +1,21 @@
 package com.zlwon.api.controller;
 
 import com.zlwon.constant.StatusCode;
+import com.zlwon.rdb.entity.Collection;
 import com.zlwon.rdb.entity.VoteActivity;
 import com.zlwon.rest.ResultData;
+import com.zlwon.server.service.CollectionService;
+import com.zlwon.server.service.RedisService;
 import com.zlwon.server.service.VoteActivityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +34,12 @@ public class TestApi extends BaseApi {
 	
 	@Autowired
 	private VoteActivityService voteActivityService;
+	
+	@Autowired
+	private CollectionService collectionService;
+	
+	@Autowired
+	private RedisService redisService;
 	
 	/**
 	 * 根据活动ID查询投票活动信息
@@ -45,5 +60,38 @@ public class TestApi extends BaseApi {
 		VoteActivity temp = voteActivityService.selectVoteActivityById(id);
 		
 		return ResultData.one(temp);
+	}
+	
+	/**
+	 * 测试添加用户
+	 * @param mobile
+	 * @return
+	 */
+	@ApiOperation(value = "测试添加用户")
+    @RequestMapping(value = "/insertTestCustomer", method = RequestMethod.GET)
+    public ResultData insertTestCustomer(@RequestParam String mobile){
+	
+		//验证参数
+		if(StringUtils.isBlank(mobile)){
+			return ResultData.error(StatusCode.INVALID_PARAM);
+		}
+		
+		//生成随机短信验证码
+		String mobile_code = String.valueOf((int)((Math.random()*9+1)*100000));
+		//将短信验证码存储入redis，命名规则mobilecode_+对应mobile符串
+		//短信验证码10分钟内有效
+		redisService.set("mobilecode_"+mobile, mobile_code,60*10,TimeUnit.SECONDS);
+		
+		String validcode = redisService.get("mobilecode_"+mobile);
+		
+		Collection temp = new Collection();
+		temp.setIid(111);
+		temp.setUid(111);
+		temp.setType(1);
+		temp.setCreateTime(new Date());
+		
+		Collection count = collectionService.insertCollection(temp);
+		
+		return ResultData.one(count);
 	}
 }
