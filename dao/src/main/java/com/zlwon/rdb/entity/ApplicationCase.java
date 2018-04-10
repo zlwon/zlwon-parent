1ff5fae9ae6fca05ce807bea0d18b6252929924e
yangy <871271816@qq.com>
2018-04-10 10:13:02 +0800

    案例实体  杨宇  2018.4.10

--- dao/src/main/java/com/zlwon/rdb/entity/ApplicationCase.java
+++ dao/src/main/java/com/zlwon/rdb/entity/ApplicationCase.java
@@ -37,6 +37,10 @@ public class ApplicationCase  implements Serializable{
 	
 	private String appProduct;  //应用产品
 	
+	private Integer industryId;  //应用行业ID
+	
+	private Integer marketId;  //应用市场ID
+	
 	private String selectRequirements;  //选材要求
 	
 	private String selectCause;  //选材原因
