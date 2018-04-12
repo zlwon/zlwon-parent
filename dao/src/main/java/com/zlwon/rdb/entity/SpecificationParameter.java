dfb2df1980ec09566946eeea4028d4cfa0e8768f
yuand <773891162@qq.com>
2018-04-12 10:28:26 +0800

    案例字段修改 袁达 2018.04.12

--- dao/src/main/java/com/zlwon/rdb/entity/SpecificationParameter.java
+++ dao/src/main/java/com/zlwon/rdb/entity/SpecificationParameter.java
@@ -10,6 +10,6 @@ public class SpecificationParameter implements  Serializable{
 
     private String name;
 
-    private Integer classType;
+    private Integer classType;//类别：商标1、基材2、填充物3、阻燃等级4、安规认证5、应用行业6、应用市场7，终端客户8，应用产品9
 
 }
