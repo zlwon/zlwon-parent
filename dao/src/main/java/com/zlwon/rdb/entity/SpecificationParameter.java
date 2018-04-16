c69e71ead68b1ecb1a691104643b9f8cd29479ca
yangy <871271816@qq.com>
2018-04-16 11:56:08 +0800

    修改实体  杨宇  2018.4.16

--- dao/src/main/java/com/zlwon/rdb/entity/SpecificationParameter.java
+++ dao/src/main/java/com/zlwon/rdb/entity/SpecificationParameter.java
@@ -10,7 +10,7 @@ public class SpecificationParameter implements  Serializable{
 
     private String name;
 
-    private Integer classType;//类别：商标1、基材2、填充物3、阻燃等级4、安规认证5、应用行业6、应用市场7，终端客户8，应用产品9
+    private Integer classType;//类别：商标1、基材2、填充物3、安规认证5、应用行业6、应用市场7，终端客户8，应用产品9
     
     private Integer parentId;//父级ID，可代表别表ID，根据类型判断(商标父级是企业用户，应用行业是应用市场的上级,安规认证也有上级)
 
