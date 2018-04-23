40eac438a09df946b595093970301187a4aaf70d
yangy <871271816@qq.com>
2018-04-23 18:47:50 +0800

    修改备注  杨宇  2018.4.23

--- dao/src/main/java/com/zlwon/rdb/entity/SysHeader.java
+++ dao/src/main/java/com/zlwon/rdb/entity/SysHeader.java
@@ -17,7 +17,7 @@ public class SysHeader {
 	
     private Integer id;  //自增ID
 
-    private Integer moduleType;  //模块类别 1：首页 2：物性表模块 3：应用案例模块
+    private Integer moduleType;  //模块类别 1：首页 2：物性列表 3：应用案例列表 4：物性详情  5：应用案例详情
 
     private String descTitle;  //描述标题
 
