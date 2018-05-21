134573c8dce709bca5ee316a269d88ce536b6ece
yangy <871271816@qq.com>
2018-05-21 15:48:12 +0800

    api整改 杨宇 2018.5.21

--- api/src/main/java/com/zlwon/api/controller/BaseApi.java
+++ api/src/main/java/com/zlwon/api/controller/BaseApi.java
@@ -1,97 +1,56 @@
 package com.zlwon.api.controller;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.alibaba.fastjson.JSON;
 import com.zlwon.rdb.entity.Customer;
 import com.zlwon.server.service.CustomerService;
-import com.zlwon.server.service.MailService;
 import com.zlwon.server.service.RedisService;
-import org.apache.commons.lang3.StringUtils;
 
-import java.util.concurrent.TimeUnit;
+import io.swagger.annotations.Api;
 
+/**
+ * 共用基类
+ * @author yangy
+ *
+ */
+
+@Api
+@RestController
+@RequestMapping("/api/base")
 public class BaseApi {
 
-	/**
-	 * 验证用户是否处于登录状态
-	 * @param entryKey
-	 * @return
-	 */
-	protected String validLoginStatus(String entryKey, RedisService redisService){
-		
-		String openId = "";
-		
-		//验证加密字符串是否存在
-		if(StringUtils.isBlank(entryKey)){
-			return "";
-		}
-		
-		//从redis中获取加密字符串的内容
-		String mdStr = redisService.get("encryKey_"+entryKey);
-		if(StringUtils.isBlank(mdStr)){
-			return "";
-		}else{
-			if(!entryKey.equals(mdStr)){
-				return "";
-			}
-		}
-		
-		String[] arr = mdStr.split("_");
-		openId = arr[0];
-		
-		return openId;
-	}
+	@Autowired
+	private RedisService redisService;
+	
+	@Autowired
+	private CustomerService customerService;
 	
 	/**
-	 * 根据openId获取redis中的用户信息
+	 * 根据openId获取用户信息
 	 * @param openId
 	 * @return
 	 */
-	protected Customer getRedisCustomer(String openId,RedisService redisService,CustomerService customerService){
+	protected Customer getRedisLoginCustomer(String openId){
 		
 		Customer user = null;
-		String jsonStr = "";
 		
-		//验证openId是否存在于redis中,命名规则openId_+对应openId字符串
-		String redisValue = redisService.get("openId_"+openId);
-		if(StringUtils.isBlank(redisValue)){  //如果缓存不存在
-			//根据openId查询数据库
-			user = customerService.selectCustomerByOpenId(openId);
-			if(user != null){
-				//将对象转化为json字符串
-				String objectJson = JSON.toJSONString(user);
-				jsonStr = objectJson;
-				//存储进redis，命名规则openId_+对应openId字符串
-				redisService.set("openId_"+openId, jsonStr,60*5,TimeUnit.SECONDS);
-			}
+		//验证加密字符串是否存在
+		if(StringUtils.isBlank(openId)){
+			return null;
+		}
+		
+		//根据openId获取redis中存储的用户信息值
+		String redisValue = redisService.get(openId);
+		if(StringUtils.isBlank(redisValue)){
+			return null;
 		}else{
-			jsonStr = redisValue;
-			//将json字符串转化为对象
-			user = JSON.parseObject(jsonStr,Customer.class); 
+			user = JSON.parseObject(redisValue,Customer.class);
 		}
 		
 		return user;
-	}
-	
-	/**
-	 * 采用创建线程方式发送注册成功邮件
-	 * @param mailTo
-	 */
-	protected void sendRegisterSuccessMail(String mailTo,MailService mailService){
-		new Thread(new Runnable() {
-            @Override
-            public void run() {
-            	//拼接发送内容
-    	    	StringBuffer text = new StringBuffer();
-    	    	text.append("<div style='text-align:left'>");
-    	    	text.append("<div>欢迎使用知料网</div>");
-    	    	text.append("<br />");
-    	    	text.append("<div>恭喜您注册知料网用户成功，您的初始密码为666666，可登陆官网后进行修改，谢谢您的使用！</div>");
-    	    	text.append("<br />");
-    	    	text.append("<div>知料网团队</div>");
-    	    	text.append("</div>");
-    			
-    			mailService.sendHtmlMail(mailTo, "知料网注册成功通知邮件", text.toString());
-            }
-        }).start();
 	}
 }
--- api/src/main/java/com/zlwon/api/controller/SpecificationApi.java
+++ api/src/main/java/com/zlwon/api/controller/SpecificationApi.java
@@ -1,27 +1,15 @@
 package com.zlwon.api.controller;
 
-import com.zlwon.constant.StatusCode;
-import com.zlwon.rdb.entity.SpecificationParameter;
-import com.zlwon.rest.ResultData;
-import com.zlwon.server.service.CharacteristicService;
-import com.zlwon.server.service.RedisService;
-import com.zlwon.server.service.SpecificationParameterService;
-import com.zlwon.server.service.SpecificationService;
-import com.zlwon.vo.characteristic.CharacteristicDetailVo;
-import com.zlwon.vo.specification.SpecificationDetailVo;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.zlwon.server.service.SpecificationService;
+
+import io.swagger.annotations.Api;
 
 /**
- * 物性表api
+ * 小程序端物性接口
  * @author yangy
  *
  */
@@ -34,160 +22,5 @@ public class SpecificationApi extends BaseApi {
 	@Autowired
 	private SpecificationService specificationService;
 	
-	@Autowired
-	private CharacteristicService characteristicService;
 	
-	@Autowired
-	private SpecificationParameterService specificationParameterService;
-	
-	@Autowired
-	private RedisService redisService;
-	
-	/**
-	 * 根据物性表ID查询物性表详情
-	 * @param id
-	 * @return
-	 */
-	@ApiOperation(value = "根据物性表ID查询物性表详情")
-    @RequestMapping(value = "/querySpecById/{id}/{entryKey}", method = RequestMethod.GET)
-	public ResultData querySpecificationById(@PathVariable Integer id,@PathVariable String entryKey){
-		
-		//验证参数
-		if(id == null || StringUtils.isBlank(entryKey)){
-			return ResultData.error(StatusCode.INVALID_PARAM);
-		}
-		
-		//验证用户
-		//String openId = entryKey;
-		String openId = validLoginStatus(entryKey,redisService);
-		/*if(StringUtils.isBlank(openId)){
-			return ResultData.error(StatusCode.MANAGER_CODE_NOLOGIN);
-		}*/
-
-		//根据物性表ID查询物性表详情
-		//Specification temp = specificationService.findSpecificationById(id);
-		SpecificationDetailVo temp = specificationService.findSpecDetailById(id);
-		
-		if(temp != null){
-			//根据填充材质字符串查询填充材质
-			if(StringUtils.isNotBlank(temp.getFiller())){
-				List<SpecificationParameter> fillerList = specificationParameterService.findSpecificationParameterByIdStr(temp.getFiller());
-				//处理拼接
-				if(fillerList != null && fillerList.size() > 0){
-					temp.setFillerList(fillerList);
-					String fillerStr = "";
-					for(SpecificationParameter fillTemp : fillerList){
-						fillerStr = fillerStr + fillTemp.getName() + ",";
-					}
-					fillerStr = fillerStr.substring(0, fillerStr.length()-1);
-					temp.setFiller(fillerStr);
-				}
-			}
-			
-			//根据安规认证字符串查询安规认证
-			if(StringUtils.isNotBlank(temp.getSafetyCert())){
-				List<SpecificationParameter> safetyCertificyList = specificationParameterService.findSpecificationParameterByIdStr(temp.getSafetyCert());
-				//处理拼接
-				if(safetyCertificyList != null && safetyCertificyList.size() > 0){
-					temp.setSafetyCertificyList(safetyCertificyList);
-					String safetyCertificyStr = "";
-					for(SpecificationParameter safetyTemp : safetyCertificyList){
-						safetyCertificyStr = safetyCertificyStr + safetyTemp.getName() + ",";
-					}
-					safetyCertificyStr = safetyCertificyStr.substring(0, safetyCertificyStr.length()-1);
-					temp.setSafetyCert(safetyCertificyStr);
-				}
-			}
-		}
-		
-		return ResultData.one(temp);
-	}
-	
-	/**
-	 * 根据物性表规格名查询物性表详情
-	 * @param name
-	 * @return
-	 */
-	@ApiOperation(value = "根据物性表规格名查询物性表详情")
-    @RequestMapping(value = "/querySpecByName/{name}/{entryKey}", method = RequestMethod.GET)
-	public ResultData querySpecificationByName(@PathVariable String name,@PathVariable String entryKey){
-		
-		//验证参数
-		if(StringUtils.isBlank(name) || StringUtils.isBlank(entryKey)){
-			return ResultData.error(StatusCode.INVALID_PARAM);
-		}
-		
-		//验证用户
-		//String openId = entryKey;
-		String openId = validLoginStatus(entryKey,redisService);
-		/*if(StringUtils.isBlank(openId)){
-			return ResultData.error(StatusCode.MANAGER_CODE_NOLOGIN);
-		}*/
-		
-		//根据物性表规格名查询物性表详情
-		//Specification temp = specificationService.findSpecificationByName(name);
-		SpecificationDetailVo temp = specificationService.findSpecDetailByName(name);
-		
-		if(temp != null){
-			//根据填充材质字符串查询填充材质
-			if(StringUtils.isNotBlank(temp.getFiller())){
-				List<SpecificationParameter> fillerList = specificationParameterService.findSpecificationParameterByIdStr(temp.getFiller());
-				//处理拼接
-				if(fillerList != null && fillerList.size() > 0){
-					temp.setFillerList(fillerList);
-					String fillerStr = "";
-					for(SpecificationParameter fillTemp : fillerList){
-						fillerStr = fillerStr + fillTemp.getName() + ",";
-					}
-					fillerStr = fillerStr.substring(0, fillerStr.length()-1);
-					temp.setFiller(fillerStr);
-				}
-			}
-			
-			//根据安规认证字符串查询安规认证
-			if(StringUtils.isNotBlank(temp.getSafetyCert())){
-				List<SpecificationParameter> safetyCertificyList = specificationParameterService.findSpecificationParameterByIdStr(temp.getSafetyCert());
-				//处理拼接
-				if(safetyCertificyList != null && safetyCertificyList.size() > 0){
-					temp.setSafetyCertificyList(safetyCertificyList);
-					String safetyCertificyStr = "";
-					for(SpecificationParameter safetyTemp : safetyCertificyList){
-						safetyCertificyStr = safetyCertificyStr + safetyTemp.getName() + ",";
-					}
-					safetyCertificyStr = safetyCertificyStr.substring(0, safetyCertificyStr.length()-1);
-					temp.setSafetyCert(safetyCertificyStr);
-				}
-			}
-		}
-		
-		return ResultData.one(temp);
-	}
-	
-	/**
-	 * 根据物性规格ID查询标签详情
-	 * @param specId
-	 * @param entryKey
-	 * @return
-	 */
-	@ApiOperation(value = "根据物性规格ID查询标签详情")
-    @RequestMapping(value = "/queryCharacteristicBySpecId/{specId}/{entryKey}", method = RequestMethod.GET)
-	public ResultData queryCharacteristicBySpecId(@PathVariable Integer specId,@PathVariable String entryKey){
-		
-		//验证参数
-		if(specId == null || StringUtils.isBlank(entryKey)){
-			return ResultData.error(StatusCode.INVALID_PARAM);
-		}
-		
-		//验证用户
-		//String openId = entryKey;
-		String openId = validLoginStatus(entryKey,redisService);
-		/*if(StringUtils.isBlank(openId)){
-			return ResultData.error(StatusCode.MANAGER_CODE_NOLOGIN);
-		}*/
-		
-		//根据物性规格ID查询标签详情
-		List<CharacteristicDetailVo> list = characteristicService.findCharacteristicGroupBySepcId(specId);
-		
-		return ResultData.one(list);
-	}
 }
