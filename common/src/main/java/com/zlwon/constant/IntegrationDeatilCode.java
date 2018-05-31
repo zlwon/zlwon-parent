45545c5a68ffe987e19efeb116a1305d547f04fc
yangy <871271816@qq.com>
2018-05-31 17:44:06 +0800

    添加物性标签  杨宇  2018.5.31

--- common/src/main/java/com/zlwon/constant/IntegrationDeatilCode.java
+++ common/src/main/java/com/zlwon/constant/IntegrationDeatilCode.java
@@ -7,6 +7,7 @@ public enum IntegrationDeatilCode {
 	INSERT_QUOTATION(10003,"新增报价单",5),
 	NEW_REGISTER(10004,"新注册",10),
 	AUTH_SUCCESS(10005,"完善资料申请认证",5),
+	INSERT_SPEC_CHARACTERISTIC(10006,"新增物性标签",3),
 	
 	SEND_SPEC_PDF_MYEMAIL(20001,"物性PDF发我邮箱",-1),
 	CONSULTE_EMAIL_QUOTATION(20002,"咨询物性报价",-1),
