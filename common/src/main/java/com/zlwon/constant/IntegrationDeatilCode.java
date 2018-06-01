f365b41fcc188a81359ab2c01ea34ef0946a1fd1
yangy <871271816@qq.com>
2018-06-01 11:24:21 +0800

    点赞相关code  杨宇  2018.6.1

--- common/src/main/java/com/zlwon/constant/IntegrationDeatilCode.java
+++ common/src/main/java/com/zlwon/constant/IntegrationDeatilCode.java
@@ -10,13 +10,17 @@ public enum IntegrationDeatilCode {
 	INSERT_SPEC_CHARACTERISTIC(10006,"新增物性标签",3),
 	PASSIVE_INVITATE_ANSWER(10007,"被邀请回答提问",1),
 	INSERT_EDIT_APPLICATION(10008,"编辑案例",10),
+	SPEC_CHARACTERISTIC_LIKE(10009,"物性标签点赞",1),
+	ANSWER_LIKE(10010,"回答点赞",1),
 	
 	SEND_SPEC_PDF_MYEMAIL(20001,"物性PDF发我邮箱",-1),
 	CONSULTE_EMAIL_QUOTATION(20002,"咨询物性报价",-1),
 	REJECT_QUESTION(20003,"驳回提问",-3),
 	REJECT_ANSWER(20004,"驳回回答",-5),
 	REJECT_CHARACTERISTIC(20005,"驳回物性标签",-3),
-	INVITATE_ANSWER(20006,"邀请用户回答提问",-1);
+	INVITATE_ANSWER(20006,"邀请用户回答提问",-1),
+	SPEC_CHARACTERISTIC_CANCEL_LIKE(20007,"取消物性标签点赞",-1),
+	ANSWER_CANCEL_LIKE(20008,"取消回答点赞",-1);
 	
 	private Integer code;
     private String message;
